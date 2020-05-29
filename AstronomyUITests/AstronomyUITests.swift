4878673a2b03244fe6e145256ef46bb95c7e0173
Marc <marc.daniel.jacques@gmail.com>
2020-05-28 22:31:47 -0400

    -fixed some photo_manifest issues

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -9,16 +9,26 @@
 import XCTest
 
 class AstronomyUITests: XCTestCase {
+    
+    // MARK: Properties
+    
+    let app = XCUIApplication()
+    
+   
+    
+    var saveButton: XCUIElement {
+                    let button = app.buttons["PhotoDetailViewController.SaveButton"]
+                    XCTAssertTrue(button.exists)
+                    XCTAssertTrue(button.isHittable)
+                    return button
+                }
 
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        let app = XCUIApplication()
-        app.launchArguments = ["UITesting"]
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    app.launchArguments = ["UITesting"]
+    app.launch()
+    continueAfterFailure = false
     }
 
     override func tearDown() {
@@ -35,17 +45,6 @@ class AstronomyUITests: XCTestCase {
     }
     
     func testSavingAPhoto() {
-        let app = XCUIApplication()
-        app.launchArguments = ["UITesting"]
-        app.launch()
-        
-        var saveButton: XCUIElement {
-            let button = app.buttons["PhotoDetailViewController.SaveButton"]
-            XCTAssertTrue(button.exists)
-            XCTAssertTrue(button.isHittable)
-            return button
-        }
-        
         let collectionView = app.collectionViews.firstMatch
         collectionView.cells.firstMatch.tap()
         saveButton.tap()
